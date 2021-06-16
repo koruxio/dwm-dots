42178dcfc82c88446e920f815db8ddb69c9b793e
sarosx <dev.machine01@protonmail.com>
2021-06-16 21:21:13 +0530

    changed terminal and install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-sudo pacman -S arc-gtk-theme transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp nemo gvfs file-roller neofetch feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot alacritty ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
+sudo pacman -S arc-gtk-theme transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp nemo gvfs file-roller neofetch feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot kitty ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
-sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions 
-sudo pacman -S telegram-desktop nodejs npm
+sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions telegram-desktop nodejs npm --needed --noconfirm
 
 chsh -s /bin/zsh 
 
@@ -9,13 +8,3 @@ cp -r .config .vimrc .zshrc .vim .xinitrc $HOME
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-cd $HOME/.config/dwm-6.2
-
-sudo make clean install
-
-cd $HOME/.config/dwm-6.2/slstatus
-
-sudo make clean install
-
-reboot
