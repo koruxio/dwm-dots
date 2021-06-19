bb52e232414c9f4549d86c940b6e9f9402044d16
sarosx <dev.machine01@protonmail.com>
2021-06-19 23:07:51 +0530

    added i3lock

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S arc-gtk-theme sxhkd rofi transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp nemo gvfs file-roller neofetch dunst feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot kitty ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
+sudo pacman -S arc-gtk-theme sxhkd rofi i3lock transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp nemo gvfs file-roller neofetch dunst feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot kitty ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
 sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions telegram-desktop nodejs npm --needed --noconfirm
 
