173a518d1a5a4b1be7d97a069770d1a06ebab7c9
sarosx <dev.machine01@protonmail.com>
2021-07-06 16:57:43 +0530

    script updated

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
-sudo pacman -S arc-gtk-theme sxhkd newsboat torbrowser-launcher rofi i3lock transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp pcmanfm-gtk3 gvfs file-roller neofetch dunst feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot kitty cmus eog evince ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
+sudo pacman -S arc-gtk-theme sxhkd newsboat torbrowser-launcher rofi i3lock transmission-gtk mpv firefox xf86-input-libinput gvfs-mtp pcmanfm-gtk3 gvfs file-roller neofetch dunst feh ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot kitty cmus eog evince zsh zsh-syntax-highlighting zsh-autosuggestions telegram-desktop nodejs npm ttf-nerd-fonts-symbols ttf-jetbrains-mono wget xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
 sudo pacman -S zsh zsh-syntax-highlighting zsh-autosuggestions telegram-desktop nodejs npm --needed --noconfirm
 
 chsh -s /bin/zsh 
 
-cp -r .config .vimrc .zshrc .vim .xinitrc $HOME
+cp -r .config .zshrc .xinitrc $HOME
+
+cd $HOME
+
+git clone https://github.com/sarosx/vim-configs
+
+cd vim-configs
+
+cp -r .vimrc .vim $HOME
+
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
